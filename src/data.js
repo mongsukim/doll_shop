06cb08a5a8ad011bae15fbbe1f8c392ba3056bd3
Hyesu <developermong2@gmail.com>
2021-04-26 17:52:49 +0900

    리액트 강의 실습 예제 [미니 쇼핑몰 만들기]

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id : 0,
+    title : "인형1",
+    content : "Born in France",
+    price : 120000
+  },
+
+  {
+    id : 1,
+    title : "인형2",
+    content : "Born in Seoul",
+    price : 110000
+  },
+
+  {
+    id : 2,
+    title : "Grey Yordan",
+    content : "Born in the States",
+    price : 130000
+  }
+] 
